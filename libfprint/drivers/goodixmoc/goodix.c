98f5ad1f808b61b0eadba1169ddafa1a2fc0c222
SirCipherz <sircipherz@mail.com>
2024-09-04 16:31:06 +0200

    Add support for goodix 689a

--- libfprint/drivers/goodixmoc/goodix.c
+++ libfprint/drivers/goodixmoc/goodix.c
@@ -1639,6 +1639,7 @@ static const FpIdEntry id_table[] = {
   { .vid = 0x27c6,  .pid = 0x659C,  },
   { .vid = 0x27c6,  .pid = 0x6A94,  },
   { .vid = 0x27c6,  .pid = 0x6512,  },
+  { .vid = 0x27c6,  .pid = 0x689A,  },
   { .vid = 0,  .pid = 0,  .driver_data = 0 },   /* terminating entry */
 };
 
